097008a07314c6731de79120b39a3772b9b8a180
MO-Nabeel <info.monabeel@gmail.com>
2025-03-21 23:22:53 +0530

    message

--- favserieses/favserieses/settings.py
+++ favserieses/favserieses/settings.py
@@ -78,10 +78,8 @@ WSGI_APPLICATION = 'favserieses.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'favorite_serieses',
-        'USER': 'root',
-        'PASSWORD': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
